1f05fbe7edf77b99860e400e4060208e2e805b6e
Zach S.L <zach.s.ledford@gmail.com>
2020-02-20 14:56:53 -0500

    added confirm funny intro

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,16 @@
-var gundamNames = ["Freedom", "Zaku", "WingZero", "Astray", "Sazabi", "GM"]
+// Funny confirm intro
+var game = confirm ("Do you want to play a game!") 
+    if (game){
+        alert("Get Ready To Lose!")
+    }
+    else {
+        alert("TO LATE TO GO BACK NOW!!!")
+    }
+
+
+
+// Startin Java Code
+var gundamNames = ["Freedom", "Zaku", "WingZero", "Astray", "Sazabi", "GM",""]
 
 for (var i=0; i<gundamNames.length; i++){
     console.log(gundamNames[i])
