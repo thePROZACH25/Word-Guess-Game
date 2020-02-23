fbdc5f5c96f3bea63b653460487e40105873beb5
Zach S.L <zach.s.ledford@gmail.com>
2020-02-23 08:45:55 -0500

    first game var

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,19 @@
 // Funny confirm intro
-var game = confirm ("Do you want to play a game!") 
-    if (game){
-        alert("Get Ready To Lose!")
-    }
-    else {
-        alert("TO LATE TO GO BACK NOW!!!")
-    }
+
+// var game = confirm ("Do you want to play a game!") 
+//     if (game){
+//         alert("Get Ready To Lose!")
+//     }
+//     else {
+//         alert("TO LATE TO GO BACK NOW!!!")
+//     }
 
 
 
 // Startin Java Code
 var gundamNames = ["Freedom", "Zaku", "WingZero", "Astray", "Sazabi", "GM",""]
+
+var chosenWord = ""
 
 for (var i=0; i<gundamNames.length; i++){
     console.log(gundamNames[i])
