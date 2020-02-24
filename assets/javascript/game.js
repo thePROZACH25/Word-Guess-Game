a7a28d096ad37470766ad02ff5426cd23ad03c9f
Zach S.L <zach.s.ledford@gmail.com>
2020-02-23 22:21:46 -0500

    added Jumbotron

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,42 @@
-// Funny confirm intro
+// // Funny confirm intro
 
-// var game = confirm ("Do you want to play a game!") 
-//     if (game){
-//         alert("Get Ready To Lose!")
-//     }
-//     else {
-//         alert("TO LATE TO GO BACK NOW!!!")
-//     }
+// var game = confirm("Do you want to play a game!");
+// if (game) {
+//   alert("Get Ready To Lose!");
+// } else {
+//   alert("TO LATE TO TURN BACK NOW!!!");
+// }
 
+// Starting Game Var
+// ===========================================================================
+var gundamNames = ["Freedom", "Zaku", "WingZero", "Astray", "Sazabi", "GM", ""];
+var guessedWord = "";
+var lettersInGuessWord = [];
+var blankLetters = 0;
+var letterSpots = [];
+var wrongGuesses = [];
 
+// Counters
+var wins = 0;
+var loses = 0;
+var guesses = 9;
 
-// Startin Java Code
-var gundamNames = ["Freedom", "Zaku", "WingZero", "Astray", "Sazabi", "GM",""]
+// Game Functions
+// ===========================================================================
 
-var chosenWord = ""
+// Game Start
+function startGame() {
+  guesses = 9;
+  guessedWord = gundamNames[Math.floor(Math.random() * gundamNames.length)];
+  lettersInGuessWord = guessedWord.split("");
+  blankLetters = lettersInGuessWord.length;
+  console.log(guessedWord);
 
-for (var i=0; i<gundamNames.length; i++){
-    console.log(gundamNames[i])
+  letterSpots = [];
+  wrongGuesses = [];
+
+  for (var i = 0; i < blankLetters; i++){
+      letterSpots.push("_");
+  }
+  console.log(letterSpots);
 }
